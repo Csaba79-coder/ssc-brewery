f971d15101ea494ce19156a1cdcab60e6c1cffea
Csaba Vadász <77699103+Csaba79-coder@users.noreply.github.com>
2022-02-08 15:51:08 +0100

    Spring Data JPA Repositories

--- src/main/java/guru/sfg/brewery/domain/security/User.java
+++ src/main/java/guru/sfg/brewery/domain/security/User.java
@@ -1,8 +1,15 @@
 package guru.sfg.brewery.domain.security;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.Set;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 public class User {
 
@@ -13,14 +20,22 @@ public class User {
     private String username;
     private String password;
 
+    @Singular // this one needs because of the set!
     @ManyToMany(cascade = CascadeType.MERGE)
     @JoinTable(name = "user_authority",
             joinColumns = {@JoinColumn(name = "USER_ID", referencedColumnName = "ID")},
             inverseJoinColumns = {@JoinColumn(name = "AUTHORITY_ID", referencedColumnName = "ID")})
     private Set<Authority> authorities;
 
+    @Builder.Default // to pick up default value else: it will be null!!!
     private Boolean accountNonExpired = true;
+
+    @Builder.Default
     private Boolean accountNonLocked = true;
+
+    @Builder.Default
     private Boolean credentialsNonExpired = true;
+
+    @Builder.Default
     private Boolean enabled = true;
 }
